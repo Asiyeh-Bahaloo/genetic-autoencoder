968b6ea1919056de86853c7c5628664583e6c150
Asiyeh <asiyeh.bahaloo@gmail.com>
2023-01-11 18:23:43 +0330

    add init  DocStrings

--- core/models.py
+++ core/models.py
@@ -4,15 +4,35 @@ import torch.nn.functional as F
 
 
 class TiedLinearLayer(nn.Module):
+    """A tied linear layer with Xavier initialization."""
+
     def __init__(self, inp, out):
+
         super().__init__()
         self.param = nn.Parameter(torch.zeros(out, inp))
         self.reset_parameters()
 
     def reset_parameters(self):
+        """Reset the parameters to have Xavier uniform distribution."""
         nn.init.xavier_uniform_(self.param)
 
     def forward(self, input, transpose):
+        """The forward function for the layer. The transpose parameter indicates whether to use
+         the layer in the forward path of the decoder or encoder.
+
+        Parameters
+        ----------
+        input : torch.tensor
+            The input tensor for the layer.
+        transpose : bool
+            If false, the layer is in the decoder part; otherwise, the layer is in the encoder
+            part, and the weights need to be transposed.
+
+        Returns
+        -------
+        torch.tensor
+            The output of the layer.
+        """
         if transpose is False:
             output = F.linear(input, self.param)
         else:
@@ -21,11 +41,25 @@ class TiedLinearLayer(nn.Module):
 
 
 class SingleTiedAutoEncoder(nn.Module):
+    """A one layer tied auto encoder using TiedLinearLayer."""
+
     def __init__(self, inp, out):
         super().__init__()
         self.layers = TiedLinearLayer(inp, out)
 
     def forward(self, input):
+        """The forward function with one single layer for decoder and encoder.
+
+        Parameters
+        ----------
+        input : torch.tensor
+            The input tensor for the module.
+
+        Returns
+        -------
+        torch.tensor
+            The output of the module.
+        """
         x = torch.flatten(input, start_dim=1)
         x = self.layers(x, transpose=False)
         x = self.layers(x, transpose=True)
@@ -33,17 +67,35 @@ class SingleTiedAutoEncoder(nn.Module):
 
 
 class TiedAutoEncoder(nn.Module):
+    """A tied auto encoder with arbitrary layer numbers, layer shapes, and nonlinearity between
+    them. the building block is TiedLinearLayer.
+    """
+
     def __init__(self, shape_list, nonlinearity=torch.relu):
         super().__init__()
         self.layers = nn.ModuleList([])
         self.nonlinearity = nonlinearity
         self.shape_list = shape_list
+
+        # add TiedLinearLayer with desired count and shapes to the model
         for i in range(len(self.shape_list) - 1):
             self.layers.append(
                 TiedLinearLayer(self.shape_list[i], self.shape_list[i + 1])
             )
 
     def forward(self, input):
+        """The forward function that creates the autoencoder output.
+
+        Parameters
+        ----------
+        input : torch.tensor
+            The input tensor for the autoencoder.
+
+        Returns
+        -------
+        torch.tensor, torch.tensor
+            The encoded features, the output of the autoencoder.
+        """
         x = torch.flatten(input, start_dim=1)
         # encode
         for layer in self.layers:
@@ -60,11 +112,28 @@ class TiedAutoEncoder(nn.Module):
 
 
 class GeneticTiedAutoEncoder(TiedAutoEncoder):
+    """A TiedAutoEncoder which enables two primary genetic functions."""
+
     def __init__(self, shape_list, nonlinearity=torch.relu):
         super().__init__(shape_list, nonlinearity)
 
     @torch.no_grad()
     def crossover(self, parent, layer_key, p):
+        """Create a new offspring by applying one-point crossover to parents with probability p.
+        Parameters
+        ----------
+        parent : TiedAutoEncoder
+            The second applicant for the crossover.
+        layer_key : str
+            The name of the layer that contains the learning chromosome.
+        p : float
+            The probability of crossover. Means how many of the weights should taken from one parent.
+
+        Returns
+        -------
+        GeneticTiedAutoEncoder
+            A new model that contains the offspring's weights for the particular chromosome.
+        """
         parent1 = self.state_dict()[layer_key]
         parent2 = parent.state_dict()[layer_key]
         assert parent1.shape == parent2.shape, "shape of parents should match"
@@ -78,6 +147,15 @@ class GeneticTiedAutoEncoder(TiedAutoEncoder):
 
     @torch.no_grad()
     def mutation(self, layer_key, p):
+        """Change (in place) the weights of the chromosomes to zero randomly with probability p.
+
+        Parameters
+        ----------
+        layer_key : str
+            The name of the layer that contains the learning chromosome.
+        p : float
+            The probability for each gene to turn to zero.
+        """
         chromosome = self.state_dict()[layer_key]
         weights_mask = torch.FloatTensor(chromosome.shape).uniform_(0, 1) > p
         self.state_dict()[layer_key] = chromosome * weights_mask
--- core/train.py
+++ core/train.py
@@ -16,11 +16,28 @@ from models import TiedAutoEncoder, GeneticTiedAutoEncoder
 
 
 def train(model, device, train_loader, optimizer, epoch, loss_fn=F.mse_loss):
+    """Train the model with stochastic gradient descent for one epoch.
+
+    Parameters
+    ----------
+    model : nn.Module
+        The model we want to train.
+    device : torch.device
+        Specify which device (cpu or GPU) should be used for training.
+    train_loader : torch.DataLoader
+        Data loader for the train dataset.
+    optimizer : torch.optimizer
+        Optimizer for the model.
+    epoch : int
+        Epoch number.
+    loss_fn : function, optional
+        Loss function, by default F.mse_loss
+    """
     model.train()
     for batch_idx, (data, _) in enumerate(train_loader):
         data = data.to(device)
         optimizer.zero_grad()
-        encoded_feats, reconstructed_output = model(data)
+        _, reconstructed_output = model(data)
         target = torch.flatten(data, start_dim=1)
         loss = loss_fn(reconstructed_output, target)
         loss.backward()
@@ -34,6 +51,22 @@ def train(model, device, train_loader, optimizer, epoch, loss_fn=F.mse_loss):
 
 @torch.no_grad()
 def evaluate_loss(model, data_loader, device):
+    """Evaluate the model and calculate the MSE loss.
+
+    Parameters
+    ----------
+    model : nn.Module_
+        Model for the evaluation purposes.
+    data_loader : torch.DataLoader
+        Data loader for a specific dataset (train or test).
+    device : torch.device
+        Specify which device (cpu or GPU) should be used for training.
+
+    Returns
+    -------
+    float
+        MSE loss of the model on the whole dataset.
+    """
     val_loss = 0
     for data, _ in data_loader:
         data = data.to(device)
@@ -47,8 +80,25 @@ def evaluate_loss(model, data_loader, device):
 
 
 def layer_wise_train(model, device, train_loader, lr, epoch, loss_fn=F.mse_loss):
-    #     shape_list = [784, 500, 250, 100, 50]
-    shape_list = model.shape_list
+    """Train the whole (TiedAutoEncoder) model in a layer-wise manner using the train function
+    at each epoch.
+
+    Parameters
+    ----------
+    model : TiedAutoEncoder
+        The model we want to train.
+    device : torch.device
+        Specify which device (cpu or GPU) should be used for training.
+    train_loader : torch.DataLoader
+        Data loader for the train dataset.
+    lr : float
+        Learning rate for optimizer.
+    epoch : int
+        The total number of train epochs.
+    loss_fn : function, optional
+        Loss function, by default F.mse_loss
+    """
+    shape_list = model.shape_list  # shape_list = [784, 500, 250, 100, 50]
     current_shape_list = []
     weight_state_dict = {}
 
@@ -56,6 +106,7 @@ def layer_wise_train(model, device, train_loader, lr, epoch, loss_fn=F.mse_loss)
         current_shape_list.append(layer_shape)
         if len(current_shape_list) < 2:
             continue
+        # recreate model to have just one learnable layer
         model = TiedAutoEncoder(current_shape_list, nonlinearity=torch.relu)
         print("training .... \n", model)
         # load prev weights
@@ -83,6 +134,35 @@ def train_genetic_model(
     loss_fn=F.mse_loss,
     device="cpu",
 ):
+    """Train the last layer in a GeneticTiedAutoEncoder model using a genetic algorithm.
+
+    Parameters
+    ----------
+    shape_list : python list
+        A list containing the shape of distinct autoencoder layers.
+    train_loader : torch.DataLoader
+        Data loader for the train dataset.
+    pop_size : int
+        Population size of chromosomes.
+    generations : int
+        The total training generations.
+    prev_weights : dict, optional
+        Previous layer weights that should be loaded into the model, by default {}
+    finetune_epoch : int, optional
+        The number of epochs we finetune chromosomes using the stochastic gradient descent
+        algorithm, by default 3
+    finetune_lr : float, optional
+        Learning rate for finetuning, by default 0.01
+    loss_fn : function, optional
+        Loss function for finetuning, by default F.mse_loss
+    device : str, optional
+        "cpu" or "gpu", by default "cpu"
+
+    Returns
+    -------
+    GeneticTiedAutoEncoder
+        A GeneticTiedAutoEncoder containing the best learned weights.
+    """
 
     # Generate choromosomes
     print(f"\nGenerated {pop_size} models with {shape_list} layer size! ")
@@ -114,7 +194,7 @@ def train_genetic_model(
         for model in models:
             fitness.append(1 / evaluate_loss(model, train_loader, device))
 
-        # Select 5 best models (/choromosomes)
+        # Select 5 best models (/chromosomes)
         fit_arg = np.argsort(fitness)
         selected_models = [models[f] for f in fit_arg[: pop_size // 2]]
         print(f"Selected {pop_size // 2} Top best chromosomes")
@@ -169,6 +249,34 @@ def layerwise_genetic_train(
     loss_fn=F.mse_loss,
     device="cpu",
 ):
+    """Builds and trains the whole GeneticTiedAutoEncoder model in a layer-wise manner using
+    the genetic algorithm.
+
+    Parameters
+    ----------
+    shape_list : python list
+        A list containing the shape of distinct autoencoder layers.
+    train_loader : torch.DataLoader
+        Data loader for the train dataset.
+    pop_size : int
+        Population size of chromosomes.
+    generations : int
+        The total training generations.
+    finetune_epoch : int, optional
+        The number of epochs we finetune chromosomes using the stochastic gradient descent
+        algorithm, by default 3
+    finetune_lr : float, optional
+        Learning rate for finetuning, by default 0.01
+    loss_fn : function, optional
+        Loss function for finetuning, by default F.mse_loss
+    device : str, optional
+        "cpu" or "gpu", by default "cpu"
+
+    Returns
+    -------
+    GeneticTiedAutoEncoder
+       A GeneticTiedAutoEncoder containing the best learned weights.
+    """
     current_shape_list = []
     weight_state_dict = {}
 
